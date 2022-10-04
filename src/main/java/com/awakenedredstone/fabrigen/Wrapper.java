bced2f952ab2c2956e5d048438304bec404a93f1
Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>
2022-10-04 17:29:15 -0300

    Update master (#3)
    * Initial rewrite
    
    * Code bump
    
    * Update cache
    
    * Code bump
    
    Signed-off-by: Awakened-Redstone <awakenedredstone@gmail.com>
    
    * Code bump
    
    Signed-off-by: Awakened-Redstone <awakenedredstone@gmail.com>
    
    * Add error screen when an exception is thrown when starting JavaFX.
    
    * Fix Main Class not being corrected
    
    Signed-off-by: Awakened-Redstone <awakenedredstone@gmail.com>
    
    * Version bump
    
    Signed-off-by: Awakened-Redstone <awakenedredstone@gmail.com>
    
    * Code cleanup
    
    Signed-off-by: Awakened-Redstone <awakenedredstone@gmail.com>
    
    * Code cleanup
    
    Signed-off-by: Awakened-Redstone <awakenedredstone@gmail.com>
    
    * Code cleanup
    
    Signed-off-by: Awakened-Redstone <awakenedredstone@gmail.com>
    
    Signed-off-by: Awakened-Redstone <awakenedredstone@gmail.com>

--- src/main/java/com/awakenedredstone/fabrigen/Wrapper.java
+++ src/main/java/com/awakenedredstone/fabrigen/Wrapper.java
@@ -1,7 +1,56 @@
 package com.awakenedredstone.fabrigen;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 public class Wrapper {
     public static void main(String[] args) {
-        JavaFX.init(args);
+        try {
+            JavaFX.init(args);
+        } catch (Exception e) {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream out = new PrintStream(baos);
+
+            JFrame frame = new JFrame();
+            JPanel panel = new JPanel();
+
+            JTextArea textArea = new JTextArea();
+            textArea.setEditable(false);
+            textArea.setLineWrap(true);
+            textArea.setBackground(frame.getBackground());
+            textArea.setSize(new Dimension(400, 300));
+            textArea.setMaximumSize(new Dimension(400, 300));
+            textArea.setMinimumSize(new Dimension(400, 300));
+
+            e.printStackTrace(out);
+            ByteArrayInputStream in = new ByteArrayInputStream(baos.toByteArray());
+            int n = in.available();
+            byte[] bytes = new byte[n];
+            in.read(bytes, 0, n);
+            String s = new String(bytes, StandardCharsets.UTF_8);
+            textArea.setText(s);
+
+            panel.add(textArea);
+
+            frame.setMinimumSize(new Dimension(560, 360));
+            frame.setSize(new Dimension(560, 360));
+            frame.setTitle("FabriGen fallback");
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+            frame.addComponentListener(new ComponentAdapter() {
+                public void componentResized(ComponentEvent e) {
+                    textArea.setSize(new Dimension(frame.getWidth() - 60, frame.getHeight() - 60));
+                }
+            });
+
+            frame.add(panel);
+            frame.setVisible(true);
+        }
     }
 }
